9132a677147600431f402babdc63eaacbaf6ebfa
augustobenschain <99427161+augustobenschain@users.noreply.github.com>
2022-03-31 23:16:26 -0400

    Add files via upload

---
+++ Lab6.cs
@@ -0,0 +1,34 @@
+/**       
+ *--------------------------------------------------------------------
+ * 	   File name: Homework6
+ * 	Project name: Lab6                  https://github.com/etsucs/lab6-augustobenschain
+ *--------------------------------------------------------------------
+ * Author’s name and email:	 August Obenschain - obenschain@etsu.edu				
+ *          Course-Section: CSCI-1250-002
+ *           Creation Date:	 March 24, 2022	
+ * -------------------------------------------------------------------
+ */
+
+static void ShowCharacter()
+{
+ Console.Write("Enter a string: ");
+    
+}
+
+static void CalculateRetail()
+{
+
+}
+for (int i = 80; i <= 100; i++)
+{
+ Console.WriteLine(Celcius(i));
+}
+static double Celsius(double fTemp)
+{
+    return (5.0/9.0) * (fTemp - 32);
+}
+
+static void IsPrime()
+{
+
+}
